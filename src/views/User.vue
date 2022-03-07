16ca9adc66f8f6ff54e64227cb9bf20d245e5b04
Aomori <1064643014@qq.com>
2022-03-07 16:50:14 +0800

    add User.vue

---
+++ src/views/User.vue
@@ -0,0 +1,19 @@
+<!-- 用户中心 -->
+<template>
+    <Header/>
+
+    <Footer/>
+</template>
+
+<script>
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+export default {
+    name: "User",
+    components: {Footer, Header}
+}
+</script>
+
+<style scoped>
+
+</style>,
